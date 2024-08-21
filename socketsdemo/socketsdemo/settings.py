ce1ea4970dc134e7c898fed92f0bca13d99d2b3f
codyhenry <codyhenry527@gmail.com>
2024-08-21 17:56:01 +0000

    add basic settings installed_apps

--- socketsdemo/socketsdemo/settings.py
+++ socketsdemo/socketsdemo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # * 3rd Party
+    "channels",  # ? look into channels daphne and channels-redis
+    "bootstrap5",
 ]
 
 MIDDLEWARE = [
